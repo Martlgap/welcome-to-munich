1cfe05ce11868f275540bd3ad1452ed87e2add62
Martin Knoche <Martin.Knoche@tum.de>
2025-07-09 10:59:57 +0200

    adjusted base in vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
   };
 
   if (command !== "serve") {
-    config.base = "/martinknoche/welcome-to-munich/";
+    config.base = "/welcome-to-munich/";
   }
   return config;
 });
